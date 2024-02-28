7d1b7673472d244c42e53a352c11ab91a1f63621
Md Sakibul Hasan <sakibal623@gmail.com>
2024-02-29 01:06:58 +0600

    Base structure for Starting dev.

--- webpack.common.js
+++ webpack.common.js
@@ -11,8 +11,7 @@ module.exports = {
         popup: path.resolve('./src/popup/index.tsx'),
         options: path.resolve('./src/options/options.tsx'),
         background: path.resolve('./src/background/background.ts'),
-        contentScript: path.resolve('./src/contentScript/index.tsx'),
-        newTab: path.resolve('./src/newTab/index.tsx')
+        contentScript: path.resolve('./src/contentScript/index.tsx')
     },
     module: {
         rules: [
