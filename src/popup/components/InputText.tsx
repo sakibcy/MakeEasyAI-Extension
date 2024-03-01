72d323967d42676339b0ef6624d5c56298d7d7a1
Md Sakibul Hasan <sakibal623@gmail.com>
2024-03-02 04:26:11 +0600

    Fixed size issue related input box.

--- src/popup/components/InputText.tsx
+++ src/popup/components/InputText.tsx
@@ -18,7 +18,7 @@ export default function InputText() {
         rows={4}
         name="comment"
         id="comment"
-        className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-md ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 dark:bg-gray-200"
+        className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-md ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-4 dark:bg-gray-200 text-sm"
         defaultValue={userData}
       />
     </div>
