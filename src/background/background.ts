fb0e3b94b83307c9d99f309a1b4d3234d4b4bda4
Md Sakibul Hasan <sakibal623@gmail.com>
2024-03-02 04:09:34 +0600

    Data display on the translate box based on user click.

--- src/background/background.ts
+++ src/background/background.ts
@@ -1,3 +1,29 @@
-chrome.runtime.onInstalled.addListener(() => {
-    console.log("Hello background");
-})
+chrome.contextMenus.create({
+    id: "selectAndTranslate",
+    title: "Capture Text",
+    contexts: ["selection"],
+});
+
+async function storeSelectionText(text: any) {
+    try {
+        await chrome.storage.sync.set({ userData: text });
+    } catch (error) {
+        console.error("Error saving selection text:", error);
+    }
+}
+
+chrome.contextMenus.onClicked.addListener(async (clickData: any, tab: any) => {
+    if (clickData.menuItemId == "selectAndTranslate" && clickData.selectionText) {
+        await storeSelectionText(clickData.selectionText);
+    }
+
+    chrome.tabs.sendMessage(tab.id, { action: "captureText" }, (response) => {
+        // Use the captured text from the content script response (if applicable)
+        chrome.windows.create({
+          url: "popup.html",
+          type: "popup",
+          height: 570,
+          width: 450,
+        });
+      });
+});
--- src/popup/components/InputText.tsx
+++ src/popup/components/InputText.tsx
@@ -1,17 +1,33 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 
 export default function InputText() {
+  const [userData, setUserData] = useState('');
+
+  useEffect(() => {
+    (async () => {
+      chrome.storage.sync.get(["userData"]).then((result) => {
+        setUserData(result.userData);
+      });
+    })();
+  }, [userData]);
 
   return (
-      <div className="mx-3">
-        <textarea
-          rows={4}
-          name="comment"
-          id="comment"
-          className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-md ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 dark:bg-gray-200"
-          defaultValue={''}
-        />
-      </div>
+    <div className="mx-3">
+      <textarea
+        rows={4}
+        name="comment"
+        id="comment"
+        className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-md ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 dark:bg-gray-200"
+        defaultValue={userData}
+      />
+    </div>
   )
+}
+
+const getUserData = async () => {
+  let data = await chrome.storage.sync.get(["userData"]);
+  console.log(data.userData);
+  
+  return data.userData;
 }
