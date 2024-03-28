da3581c6a813fcf1bce1afd5dc5b8be408d1852f
Md Sakibul Hasan <sakibal623@gmail.com>
2024-03-28 21:14:10 +0600

    modified the showing result card.

--- src/popup/components/ShowResult.tsx
+++ src/popup/components/ShowResult.tsx
@@ -14,7 +14,7 @@ export default function ShowResult() {
         name="comment"
         id="comment"
         readOnly={true}
-        className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-md ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 dark:bg-gray-200"
+        className="block w-full rounded-2xl border-0 py-1.5 text-gray-900 shadow-md ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 dark:bg-gray-200"
         defaultValue={data.toString()} /> : <Skeleton />}
 
     </div>
@@ -24,10 +24,14 @@ export default function ShowResult() {
 function Skeleton() {
   return (
     <div className="flex flex-col gap-4 w-full">
-      <div className="skeleton h-28 w-full"></div>
-      <div className="skeleton h-4 w-28"></div>
-      <div className="skeleton h-4 w-full"></div>
-      <div className="skeleton h-4 w-full"></div>
+      <div className="skeleton h-52 w-full bg-slate-300">
+      <div className='pt-5 pl-5 pr-5'>
+        <div className="skeleton h-4 w-full bg-red-400"></div>
+        <div className="skeleton h-4 w-full bg-red-300"></div>
+      </div>
+      </div>
+      
+      
     </div>
   )
 }
