0c86de9b6335b1a3bb0a43d509f365f01a771609
Md Sakibul Hasan <sakibal623@gmail.com>
2024-04-02 00:25:36 +0600

    created backend for select and translate.

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,13 +1,15 @@
 import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
+import { getLanguages } from "./extAPI";
 
 dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT || 3000;
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("Express + TypeScript Server");
+app.get("/languages", async (req: Request, res: Response) => {
+  const lang = await getLanguages();
+  res.send(lang)
 });
 
 app.listen(port, () => {
